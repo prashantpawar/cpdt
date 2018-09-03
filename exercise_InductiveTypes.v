3648f5db4ce1fbfe55728b9d21ec4d629edf8f26
Prashant Singh Pawar <prashantpawar@gmail.com>
2018-09-03 10:06:15 -0400

    - WIP

--- exercise_InductiveTypes.v
+++ exercise_InductiveTypes.v
@@ -142,6 +142,115 @@ Qed.
 
 End exercise3.
 
+Module exercise4.
+
+(* 4. Reimplement the second example language of Chapter 2 to use mutually inductive types instead of dependent types. That is, define two separate (non-dependent) induc- tive types nat exp and bool exp for expressions of the two different types, rather than a single indexed type. To keep things simple, you may consider only the binary opera- tors that take naturals as operands. Add natural number variables to the language, as in the last exercise, and add an “if” expression form taking as arguments one boolean expression and two natural number expressions. Define semantics and constant-folding functions for this new language. Your constant folding should simplify not just binary operations (returning naturals or booleans) with known arguments, but also “if” ex- pressions with known values for their test expressions but possibly undetermined “then” and “else” cases. Prove that constant-folding a natural number expression preserves its meaning. *)
+
+Inductive var : Set :=
+  | vi : nat -> var.
+
+Inductive nat_exp : Set :=
+ | NConst : nat -> nat_exp
+ | NPlus : nat_exp -> nat_exp -> nat_exp
+ | NTimes : nat_exp -> nat_exp -> nat_exp
+ | NVar : var -> nat_exp
+ | NIf : bool_exp -> nat_exp -> nat_exp -> nat_exp
+
+with bool_exp : Set :=
+ | BConst : bool -> bool_exp
+ | BEq : nat_exp -> nat_exp -> bool_exp
+ | BLt : nat_exp -> nat_exp -> bool_exp.
+
+Fixpoint nexpDenote (V : var -> nat) (e : nat_exp) : nat :=
+  match e with
+    | NConst n => n
+    | NPlus ne1 ne2 => plus (nexpDenote V ne1) (nexpDenote V ne2)
+    | NTimes n1 n2 => mult (nexpDenote V n1) (nexpDenote V n2)
+    | NVar v => V v
+    | NIf b neT neF => match (bexpDenote V b) with
+                          | true => nexpDenote V neT
+                          | false => nexpDenote V neF
+                       end
+  end
+
+with bexpDenote (V : var -> nat) (be : bool_exp) : bool :=
+  match be with
+    | BConst b => b
+    | BEq ne1 ne2 => beq_nat (nexpDenote V ne1) (nexpDenote V ne2)
+    | BLt ne1 ne2 => Nat.leb (nexpDenote V ne1) (nexpDenote V ne2)
+  end.
+
+Definition replaceConstantInN (ne_op : nat_exp -> nat_exp -> nat_exp) (n_op : nat -> nat -> nat) (ne1 ne2 : nat_exp) :=
+  match ne1 with
+    | NConst n1 => match ne2 with
+                    | NConst n2 => NConst (n_op n1 n2)
+                    | _ => ne_op ne1 ne2
+                  end
+    | _ => ne_op ne1 ne2
+  end.
+
+Definition replaceConstantInB (be_op : nat_exp -> nat_exp -> bool_exp) (b_op : nat -> nat -> bool) (ne1 ne2 : nat_exp) :=
+  match ne1 with
+    | NConst n1 => match ne2 with
+                    | NConst n2 => BConst (b_op n1 n2)
+                    | _ => be_op ne1 ne2
+                  end
+    | _ => be_op ne1 ne2
+  end.
+
+Fixpoint nfold (e : nat_exp) : nat_exp :=
+  match e with
+    | NConst n => NConst n
+    | NPlus ne1 ne2 => replaceConstantInN NPlus plus (nfold ne1) (nfold ne2)
+    | NTimes ne1 ne2 => replaceConstantInN NTimes mult (nfold ne1) (nfold ne2)
+    | NVar v => NVar v
+    | NIf b ne1 ne2 => 
+        match (bfold b) with
+          | BConst b => if b then (nfold ne1) else (nfold ne2)
+          | _ => NIf (bfold b) (nfold ne1) (nfold ne2)
+        end
+  end
+with bfold (e : bool_exp) : bool_exp :=
+  match e with
+    | BConst b => BConst b
+    | BEq ne1 ne2 => replaceConstantInB BEq beq_nat (nfold ne1) (nfold ne2)
+    | BLt ne1 ne2 => replaceConstantInB BLt Nat.leb (nfold ne1) (nfold ne2)
+  end.
+
+Eval simpl in nfold (NConst 42).
+
+Eval simpl in nfold (NPlus (NConst 2) (NConst 2)).
+
+Eval simpl in nfold (NIf (BEq (NPlus (NConst 2) (NConst 4)) (NConst 7)) (NConst 1) (NConst 0)).
+
+
+Theorem nfold_is_correct : forall V e,
+  nexpDenote V (nfold e) = nexpDenote V e.
+Proof.
+  intros V e.
+  induction e; crush.
+  - destruct (nfold e1); destruct (nfold e2); crush.
+  - destruct (nfold e1); destruct (nfold e2); crush.
+  - destruct b; simpl.
+    + destruct b; crush.
+    + unfold replaceConstantInB. rewrite <- IHe1. rewrite <- IHe2.
+Qed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
